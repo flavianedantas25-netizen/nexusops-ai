f504568de0d0223a9cdeb28bf2f4de6dfe6adbe9
Flaviane Dantas <flavianedantas25-netizen@users.noreply.github.com>
2026-09-13 04:40:31 -0300

    fix: use server-provided port

--- app.py
+++ app.py
@@ -1,8 +1,18 @@
+import os
+
 import uvicorn
 
+
 if __name__ == "__main__":
+    port = int(
+        os.getenv(
+            "SERVER_PORT",
+            os.getenv("PORT", "8000"),
+        )
+    )
+
     uvicorn.run(
         "backend.app.main:app",
         host="0.0.0.0",
-        port=8000,
+        port=port,
     )
